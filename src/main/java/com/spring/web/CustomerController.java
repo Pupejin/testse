ff885e2e9398b852e0dee5bbb63ce452c2f1e2a5
DaisukiDaYo <un_un00@hotmail.com>
2016-12-29 01:23:39 +0700

    add entity customer

---
+++ src/main/java/com/spring/web/CustomerController.java
@@ -0,0 +1,11 @@
+package com.spring.web;
+import com.spring.domain.Customer;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/customers")
+@Controller
+@RooWebScaffold(path = "customers", formBackingObject = Customer.class)
+public class CustomerController {
+}
